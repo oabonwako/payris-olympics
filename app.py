c8c820a31f224ff4386c097c1baed004c04f964b
Oabo <o.nwako36@gmail.com>
2024-05-11 05:42:35 +0200

    final version

--- app.py
+++ app.py
@@ -1,23 +1,50 @@
 # Import packages
 import dash
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import dash_bootstrap_components as dbc
 
-
-# Initialize the app
-app = dash.Dash(__name__,suppress_callback_exceptions=True,use_pages=True)
+app = dash.Dash(__name__,suppress_callback_exceptions=True, use_pages=True, external_stylesheets=[dbc.themes.SPACELAB])
 server = app.server
-# App layout
-app.layout = html.Div([
-    html.Div(children='2024 Payris FunOlympic Games analysis tool', style={'fontSize':50, 'textAlign':'center'}),
-    html.Div([
-        dcc.Link(page['name']+" | ", href=page['relative_path'])
-        for page in dash.page_registry.values()
+sidebar = dbc.Nav(
+            [
+                dbc.NavLink(
+                    [
+                        html.Div(page["name"], className="ms-2"),
+                    ],
+                    href=page["path"],
+                    active="exact",
+                )
+                for page in dash.page_registry.values()
+            ],
+            vertical=True,
+            pills=True,
+            className="bg-light",
+)
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.Div("2024 Payris FunOlympic Games analysis tool",
+                         style={'fontSize':50, 'textAlign':'center'}))
     ]),
+
     html.Hr(),
 
-    #Content of each page
-    dash.page_container
-])
+    dbc.Row(
+        [
+            dbc.Col(
+                [
+                    sidebar
+                ], xs=4, sm=4, md=2, lg=2, xl=2, xxl=2),
+
+            dbc.Col(
+                [
+                    dash.page_container
+                ], xs=8, sm=8, md=10, lg=10, xl=10, xxl=10)
+        ]
+    )
+], fluid=True)
+
+
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=False)
